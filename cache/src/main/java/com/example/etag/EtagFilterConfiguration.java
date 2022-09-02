bad49342ba6622ea8139b7457fbda43d7e5c174b
nailseong <nnis9857@gmail.com>
2022-09-02 13:16:42 +0900

    feat: 캐시 무효화

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.etag;
 
+import com.example.version.ResourceVersion;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +9,17 @@ import org.springframework.web.filter.ShallowEtagHeaderFilter;
 @Configuration
 public class EtagFilterConfiguration {
 
+    private final ResourceVersion version;
+
+    public EtagFilterConfiguration(final ResourceVersion version) {
+        this.version = version;
+    }
+
     @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
         final FilterRegistrationBean<ShallowEtagHeaderFilter> filter = new FilterRegistrationBean<>(
                 new ShallowEtagHeaderFilter());
-        filter.addUrlPatterns("/etag");
+        filter.addUrlPatterns("/etag", "/resources/" + version.getVersion() + "/js/index.js");
         return filter;
     }
 }

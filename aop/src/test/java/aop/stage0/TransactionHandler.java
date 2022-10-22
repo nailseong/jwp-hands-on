63ae315bb777ae5ada0096654d8615f27af013ff
nailseong <nnis9857@gmail.com>
2022-10-22 22:51:13 +0900

    test: 0단계 - JDK Proxy로 프록시 패턴 적용하기

--- aop/src/test/java/aop/stage0/TransactionHandler.java
+++ aop/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,15 +1,47 @@
 package aop.stage0;
 
+import aop.DataAccessException;
+import aop.Transactional;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class TransactionHandler implements InvocationHandler {
+
+    private final PlatformTransactionManager transactionManager;
+    private final Object target;
+
+    public TransactionHandler(final PlatformTransactionManager transactionManager, final Object target) {
+        this.transactionManager = transactionManager;
+        this.target = target;
+    }
 
     /**
      * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
      */
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+        final Method declaredMethod = target.getClass()
+                .getDeclaredMethod(method.getName(), method.getParameterTypes());
+        if (declaredMethod.isAnnotationPresent(Transactional.class)) {
+            // TODO: 2022/10/22 invoke
+            return invokeWithTransaction(method, args);
+        }
+        return method.invoke(target, args);
+    }
+
+    private Object invokeWithTransaction(final Method method, final Object[] args) {
+        final TransactionStatus transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            final Object result = method.invoke(target, args);
+            transactionManager.commit(transactionStatus);
+            return result;
+        } catch (RuntimeException | InvocationTargetException | IllegalAccessException e) {
+            transactionManager.rollback(transactionStatus);
+            throw new DataAccessException(e);
+        }
     }
 }
